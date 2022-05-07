952f302e33bbb2e0f671f9e5246037d22b7dbffc
coderslav <coderslav@gmail.com>
2022-05-07 14:06:13 +0200

    BACKEND: View and Urls in process

--- back/api_app/views.py
+++ back/api_app/views.py
@@ -1,16 +1,8 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Realty
 from .serializers import RealtySerializer
 
 # Create your views here.
-class CreateRealtyAPIView(generics.CreateAPIView):
-    queryset = Realty.objects.all()
-    serializer_class = RealtySerializer
-    
-class DetailRealtyAPIView(generics.RetrieveUpdateAPIView):
-    queryset = Realty.objects.all()
-    serializer_class = RealtySerializer
-
-class ListRealtyAPIView(generics.ListAPIView):
+class RealtyAPIViewSet(viewsets.ModelViewSet):
     queryset = Realty.objects.all()
     serializer_class = RealtySerializer
