06798381b1112c6249f10fb5d02205c6c4fdcc61
coderslav <coderslav@gmail.com>
2022-05-07 12:02:17 +0200

    BACKEND: Added Serializers and Views

---
+++ back/api_app/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Realty
+
+class RealtySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Realty
+        fields = '__all__'
--- back/api_app/views.py
+++ back/api_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Realty
+from .serializers import RealtySerializer
 
 # Create your views here.
+class CreateRealtyAPIView(generics.CreateAPIView):
+    queryset = Realty.objects.all()
+    serializer_class = RealtySerializer
+    
+class DetailRealtyAPIView(generics.RetrieveUpdateAPIView):
+    queryset = Realty.objects.all()
+    serializer_class = RealtySerializer
+
+class ListRealtyAPIView(generics.ListAPIView):
+    queryset = Realty.objects.all()
+    serializer_class = RealtySerializer
